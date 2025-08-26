93842fb19d090e88c4e0136fb6b3b7a4bede7990
Nath <udayan-kumar.nath@capgemini.com>
2025-08-27 00:50:39 +0530

    Prophet implementation

--- sales.py
+++ sales.py
@@ -11,8 +11,8 @@ df = spark.read.csv("sales_dataset.csv", header=True, inferSchema=True)
 df.show(5)
 df.printSchema()
 
-# df = df.withColumn("OrderDate", to_date(col("OrderDate"), "dd-MM-yyyy")) \
-#        .withColumn("ShipDate", to_date(col("ShipDate"), "dd-MM-yyyy"))
+df = df.withColumn("OrderDate", to_date(col("OrderDate"), "dd-MM-yyyy")) \
+       .withColumn("ShipDate", to_date(col("ShipDate"), "dd-MM-yyyy"))
 # df.groupBy().agg(sum("Sales").alias("TotalSales")).show()
 # df.groupBy("Category").agg(sum("Sales").alias("TotalSales")).orderBy(col("TotalSales").desc()).show()
 # df.groupBy("Region").agg(sum("Sales").alias("TotalSales")).orderBy(col("TotalSales").desc()).show()p
